8027d8f960dc79260d58565913eb098a1160883d
Apprentice <apprentice@dbc28.local>
2013-02-15 11:40:00 -0800

    Wrote skeleton code ToDoView

---
+++ app/views/todoview.rb
@@ -0,0 +1,20 @@
+class ToDoView 
+  def confirm_add
+    
+  end
+
+  def confirm_delete
+    
+  end
+
+  def display_header
+    
+  end
+
+  def list_tasks 
+    
+  end
+
+  def help
+  end
+end
