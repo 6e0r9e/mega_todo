e70bffbcc6bd25b2844ce5fd4505f8ba80c83705
Alyraz <alyssaravasio@gmail.com>
2013-02-15 11:21:28 -0800

    Add todo controller

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,2 +1,17 @@
+require 'sqlite3'
+
+require_relative '../models/task'
+# require_relative '../views/todo_view'
+
+DATAFILE = '../../db/tasks.sqlite3'
+
 class ToDoController
+  def initialize
+    # Task.new(:text => "walk cat").save!
+    # @view = ToDoView.new
+  end
+
+
 end
+
+mega_controller = ToDoController.new
