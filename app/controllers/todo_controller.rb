6e64af27387dda33d44b3d793eebf06259ad4873
Alyraz <alyssaravasio@gmail.com>
2013-02-15 13:14:19 -0800

    Add calls to view methods

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,37 +2,32 @@ require 'sqlite3'
 require 'pp'
 
 require_relative '../models/task'
-# require_relative '../views/todo_view'
+require_relative '../views/todo_view'
 
 class ToDoController
-
-
   def self.run!
     command = ARGV.shift
 
     case command
       when "add"
         Task.add(ARGV.join(' '))
+        ToDoView.confirm_add
       when "complete"
         Task.complete(ARGV.shift)
+        ToDoView.confirm_complete
       when "delete"
         Task.delete(ARGV)
+        ToDoView.confirm_delete
       when "list"
         PP.pp Task.find :all
-        #View.print(Task.find :all) - Add in once View is merged.
+        #ToDoView.print(Task.find :all) - Add in once ToDoView is merged.
       when "help"
+        ToDoView.help
       else
-        View.error
+        ToDoView.error
     end
   end
-
-
-
 end
 
 #temporary driver code for testing
-ToDoController.run! 
-
-
-
-
+ToDoController.run!
